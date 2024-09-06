ccdb00411b5287a0b28d211a8f13dd7ceb8a95dc
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-06 12:24:59 +0300

    updated: ask for permissions from the user

--- app/src/main/java/com/ismailmesutmujde/kotlinfirebaseinstagramclone/UploadActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinfirebaseinstagramclone/UploadActivity.kt
@@ -1,27 +1,89 @@
 package com.ismailmesutmujde.kotlinfirebaseinstagramclone
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.ismailmesutmujde.kotlinfirebaseinstagramclone.databinding.ActivityUploadBinding
 
 
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var bindingUploadActivity : ActivityUploadBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingUploadActivity = ActivityUploadBinding.inflate(layoutInflater)
         val view = bindingUploadActivity.root
         setContentView(view)
 
-    }
-
-    fun selectImageClicked(view : View) {
-
+        registerLauncher()
     }
 
     fun uploadClicked(view : View) {
 
     }
+
+    fun selectImageClicked(view : View) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            // no permission
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission") {
+                    // request permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }.show()
+            } else {
+                // request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+        } else {
+            val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            // start activity for result
+            activityResultLauncher.launch(intentToGallery)
+        }
+    }
+
+    private fun registerLauncher() {
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val intentFromResult = result.data
+                if(intentFromResult != null) {
+                    selectedPicture =  intentFromResult.data
+                    selectedPicture?.let {
+                        bindingUploadActivity.selectImage.setImageURI(it)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+            if (result) {
+                // permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            } else {
+                // permission denied
+                Toast.makeText(this@UploadActivity, "Permission needed!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+
 }
