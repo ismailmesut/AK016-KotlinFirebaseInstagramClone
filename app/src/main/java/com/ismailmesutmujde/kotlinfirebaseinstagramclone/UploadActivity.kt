31e811666e0cada3b06403bcc72aca544583356c
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-06 13:17:57 +0300

    updated: storage upload

--- app/src/main/java/com/ismailmesutmujde/kotlinfirebaseinstagramclone/UploadActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinfirebaseinstagramclone/UploadActivity.kt
@@ -9,13 +9,20 @@ import android.provider.MediaStore
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 import com.ismailmesutmujde.kotlinfirebaseinstagramclone.databinding.ActivityUploadBinding
+import java.util.UUID
 
 
 class UploadActivity : AppCompatActivity() {
@@ -24,6 +31,9 @@ class UploadActivity : AppCompatActivity() {
     private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
     var selectedPicture : Uri? = null
+    private lateinit var auth : FirebaseAuth
+    private lateinit var firestore : FirebaseFirestore
+    private lateinit var storage : FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +42,28 @@ class UploadActivity : AppCompatActivity() {
         setContentView(view)
 
         registerLauncher()
+
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
     }
 
     fun uploadClicked(view : View) {
+
+        // uuid: universal unique id
+        val uuid = UUID.randomUUID()
+        val imageName = "$uuid.jpg"
+
+        val reference = storage.reference
+        val imageReference = reference.child("images").child(imageName)
+
+        if (selectedPicture != null) {
+            imageReference.putFile(selectedPicture!!).addOnSuccessListener {
+                // download url -> firestore
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
 
     }
 
